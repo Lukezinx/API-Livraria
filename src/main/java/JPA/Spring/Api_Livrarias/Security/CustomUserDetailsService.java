0917604e0ede0f17e89f21830a7c87e8fe78a4b5
Lucas <Lucasdeapool@gamil.com>
2025-06-18 17:50:02 -0300

    Adicionando autenticaçao de cadastro de usuario

--- src/main/java/JPA/Spring/Api_Livrarias/Security/CustomUserDetailsService.java
+++ src/main/java/JPA/Spring/Api_Livrarias/Security/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package JPA.Spring.Api_Livrarias.Security;
 
-public class CustomUserDetailsService {
+import JPA.Spring.Api_Livrarias.Services.UsuarioServices;
+import JPA.Spring.Api_Livrarias.molder.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioServices services;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Usuario  usuario = services.obterPorLogin(login);
+
+        if (usuario == null){
+            throw new UsernameNotFoundException("Usuario não encontrado");
+        }
+
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().toArray(new String[usuario.getRoles().size()]))
+                .build();
+    }
 }
--- src/main/java/JPA/Spring/Api_Livrarias/Security/SecurityService.java
+++ src/main/java/JPA/Spring/Api_Livrarias/Security/SecurityService.java
@@ -1,4 +1,25 @@
 package JPA.Spring.Api_Livrarias.Security;
 
+
+import JPA.Spring.Api_Livrarias.Services.UsuarioServices;
+import JPA.Spring.Api_Livrarias.molder.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SecurityService {
+
+    private final UsuarioServices usuarioServices;
+
+    public Usuario usuarioLogado(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String login  = userDetails.getUsername();
+        return usuarioServices.obterPorLogin(login);
+    }
 }
